6ee5af690de47c92893ebd8f2dc26b74d43273a3
Dylan Smith <dsmit809@gmail.com>
2025-08-20 13:05:30 -0600

    first read day of class?

--- hellp.cpp
+++ hellp.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+// input output inclusion
 using namespace std;
 // Dylan Smith's Files 
 
+int userInt = 0;
+
 int main() {
-  cout << "Hello World!";
+  cout << "Hello World!\nMy Name is Dylan!\n";
+  cout << "Enter a Number:\n";
+//  cin.get(userInt);
+  cout << userInt;
   return 0;
 }
 
+// return 0; means it did the thing successfully
+// return 1; means something else, like an error
