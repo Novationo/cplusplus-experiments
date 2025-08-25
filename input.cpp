a25a9f4e8918c544c5145ef166c7d7b88ea502f3
Dylan Smith <dsmit809@gmail.com>
2025-08-25 12:28:05 -0600

    basic io for cpp

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello, Enter a letter: ";
+    char letter;
+    cin >> letter;
+    cout << "Letter + 1 Was: " << letter + 1 << "\n";
+    // This will output the ASCII value of the letter + 1
+    cout << (char)(letter) << " + 1";
+
+    return 0;
+}
